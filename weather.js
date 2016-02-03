80d748c338cc3ce53ee2da9c5baf60e2c28bf32f
Andrea Jackson <andrea-jackson@kellogg.northwestern.edu>
2016-02-03 06:26:41 -0600

    changes

--- weather.js
+++ weather.js
@@ -5,18 +5,18 @@ $(function() {
     // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
 
-    var markup = "<h2>Current Weather:</h2>"
+    var markup = "<h2>Current Weather</h2>"
 
-     + "<p1>" + data.currently.summary + ", the temperature is " + data.currently.temperature + " degrees, and " + data.currently.icon + " outside.</p1>"
+     + "<p2>" + data.currently.summary + ", the temperature is " + data.currently.temperature + " degrees, and " + data.currently.icon + " outside.</p2>"
         + "<h2>The 3 day forecast is:</h2>"
 
 
-        + "<h3>Monday:</h3>"
+        + "<h3>Monday</h3>"
         + "<p2> " + data.daily.data[0].icon + " and a high temperature of " + data.daily.data[0].temperatureMax + ".</p2>"
-+ "<h3>Tuesday:</h3>"
++ "<h3>Tuesday</h3>"
 
       + "<p2> " + data.daily.data[1].icon + " and a high temperature of " + data.daily.data[1].temperatureMax + ".</p2>"
-+ "<h3>Wednesday:</h3>"
++ "<h3>Wednesday</h3>"
         + "<p2> " + data.daily.data[2].icon + " and a high temperature of " + data.daily.data[2].temperatureMax + ".</p2>"
 
     // End of your code
